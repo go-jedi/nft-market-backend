ed7a32bd9f9ba7a0e78a717c572276dc725583fb
rob-bender <rob_bender@inbox.lv>
2022-11-21 00:06:10 -0800

    change sql user_create_referal

--- sql/pb/user_create_referal.sql
+++ sql/pb/user_create_referal.sql
@@ -10,6 +10,10 @@ BEGIN
 	WHERE tele_id = (js->>'tele_id')::BIGINT
 	INTO _r;
 
+	IF js->>'tele_id' = js->>'admin_referral' THEN
+		RAISE EXCEPTION 'вы не можете использовать реферальную ссылку для себя';
+	END IF;
+
 	IF _r.id ISNULL THEN
 		INSERT INTO referral(tele_id, tele_name, admin_referral)
 		VALUES((js->>'tele_id')::BIGINT, js->>'tele_name', (js->>'admin_referral')::BIGINT);
